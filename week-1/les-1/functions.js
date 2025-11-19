f7daec11e13856f7d07d1f6cd1c68772c5b2f2c8
pgm-nickvanhoecke <nick.vanhoecke@student.arteveldehs.be>
2025-11-19 15:01:53 +0100

    Practiced Functions

---
+++ week-1/les-1/functions.js
@@ -0,0 +1,65 @@
+// Oefening: multiply, division.
+
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+// Oefening: Maak een functie showCalculation die 3
+// parameters heeft. 2 getallen en een van onze functies
+
+function showCalculation(a = 0, b = 0, calculate = sum) {
+  console.log(calculate(a, b));
+}
+
+showCalculation(5, 5, multiply);
+showCalculation(5, 5, division);
+showCalculation(5, 5, sum);
+showCalculation(5, 5, subtraction);
+showCalculation();
+
+//-----------------------------------------------------------
+
+
+const name = "Nick";
+const divisions = () => [];
+const multiplier = function () {};
+
+function selectRandomStudent(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const age = 21;
+const students = ["Yme", "Arne", "Mert"];
+
+console.log(sum(age, 10));
+console.log(20);
+console.log(selectRandomStudent(students));
+
+function showMessage(message) {
+  console.log(`Answer Checker Message`, message);
+}
+
+function checkAnswer(answer, solution, callback) {
+  if (answer === solution) {
+    callback("✅");
+    return true;
+    +_;
+  }
+
+  callback("❌");
+  return false;
+}
+
+checkAnswer("Ja", "Oplossing", showMessage(message));
